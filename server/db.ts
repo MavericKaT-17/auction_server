ad9c9f928da0c5901830dc9841d4bbd7f9c14576
MavericKaT-17 <maverickat.17@gmail.com>
2026-02-17 23:54:26 +0800

    updated db.ts for dev env

--- server/db.ts
+++ server/db.ts
@@ -1,13 +1,12 @@
 import { Pool } from "pg";
 import { drizzle } from "drizzle-orm/node-postgres";
 import * as schema from "@shared/schema";
-import * as fs from 'fs';
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-ca: fs.readFileSync('/home/ec2-user/auction_server/global-bundle.pem').toString(),
-}
+    rejectUnauthorized: false  // This is the key fix!
+  }
 });
 
 export const db = drizzle(pool, { schema });
