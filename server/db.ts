b44c7dfb88d5dc00dc8ce3d819dc69c01c0a5246
EC2 Default User <ec2-user@ip-172-31-34-11.ap-northeast-3.compute.internal>
2026-02-17 15:40:14 +0000

    updated db.ts

--- server/db.ts
+++ server/db.ts
@@ -1,9 +1,13 @@
 import { Pool } from "pg";
 import { drizzle } from "drizzle-orm/node-postgres";
 import * as schema from "@shared/schema";
+import * as fs from 'fs';
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+ca: fs.readFileSync('/home/ec2-user/auction_server/global-bundle.pem').toString(),
+}
 });
 
 export const db = drizzle(pool, { schema });
