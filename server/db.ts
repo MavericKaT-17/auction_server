3dbe9aa5ba57b738a51fc74df6bb4caf480bdd94
MavericKaT-17 <maverickat.17@gmail.com>
2026-02-18 15:52:39 +0800

    hardcode db.ts for testing

--- server/db.ts
+++ server/db.ts
@@ -3,7 +3,7 @@ import { drizzle } from "drizzle-orm/node-postgres";
 import * as schema from "@shared/schema";
 
 export const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: "postgresql://postgres:auctionwebserber2026@auction-db.c1ese8wc6fc4.ap-northeast-3.rds.amazonaws.com:5432/acution_db?ssl=require",
 });
 
 export const db = drizzle(pool, { schema });
